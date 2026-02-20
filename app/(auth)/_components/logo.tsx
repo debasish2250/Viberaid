4f7dc52a81ceede30e754f0f9714da3749c071df
Debasish Rout <debasishrout225@gmail.com>
2026-02-20 23:42:39 +0530

    Viberaid

--- app/(auth)/_components/logo.tsx
+++ app/(auth)/_components/logo.tsx
@@ -11,7 +11,7 @@ export const Logo = () => {
   return (
     <div className="flex flex-col items-center gap-y-4">
       <div className="rounded-full p-1">
-        <Image src="/spooky.svg" alt="Logo" width={50} height={50} />
+        <Image src="/viberaid.svg" alt="Logo" width={50} height={50} />
       </div>
       <div className={cn(font.className, "flex flex-col items-center")}>
         <p className="text-xl font-semibold">Viberaid</p>
